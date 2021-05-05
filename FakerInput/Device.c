dad4ed3311b502f81a63673352bbe233f9f1022e
Travis Nickles <ryochan7@protonmail.com>
2021-05-05 13:34:12 -0500

    Added absolute mouse device

--- FakerInput/Device.c
+++ FakerInput/Device.c
@@ -130,6 +130,47 @@ HID_REPORT_DESCRIPTOR       G_DefaultReportDescriptor[] = {
         0xc0,                               // END_COLLECTION
 
     //
+    // Mouse report starts here
+    //
+    0x05, 0x01,                         // USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,                         // USAGE (Mouse)
+    0xa1, 0x01,                         // COLLECTION (Application)
+    0x85, REPORTID_MOUSE,               //   REPORT_ID (Mouse)
+    0x09, 0x01,                         //   USAGE (Pointer)
+    0xa1, 0x00,                         //   COLLECTION (Physical)
+    0x05, 0x09,                         //     USAGE_PAGE (Button)
+    0x19, 0x01,                         //     USAGE_MINIMUM (Button 1)
+    0x29, 0x05,                         //     USAGE_MAXIMUM (Button 5)
+    0x15, 0x00,                         //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                         //     LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                         //     REPORT_SIZE (1)
+    0x95, 0x05,                         //     REPORT_COUNT (5)
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)
+    0x95, 0x03,                         //     REPORT_COUNT (3)
+    0x81, 0x03,                         //     INPUT (Cnst,Var,Abs)
+    0x05, 0x01,                         //     USAGE_PAGE (Generic Desktop)
+    0x27, 0xff, 0xff, 0x00, 0x00,       //     LOGICAL_MAXIMUM (65535)
+    0x75, 0x10,                         //     REPORT_SIZE (16)
+    0x95, 0x01,                         //     REPORT_COUNT (1)
+    0x55, 0x0F,                         //     UNIT_EXPONENT (-1)
+    0x65, 0x11,                         //     UNIT (cm,SI Linear)
+    0x35, 0x00,                         //     PHYSICAL_MINIMUM (0)
+    0x45, 0x00,                         //     PHYSICAL_MAXIMUM (0)
+    0x09, 0x30,                         //     USAGE (X)
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)
+    0x09, 0x31,                         //     USAGE (Y)
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)
+    0x05, 0x01,                         //     Usage Page (Generic Desktop)
+    0x09, 0x38,                         //     Usage (Wheel)
+    0x15, 0x81,                         //     Logical Minimum (-127)
+    0x25, 0x7F,                         //     Logical Maximum (127)
+    0x75, 0x08,                         //     Report Size (8)
+    0x95, 0x01,                         //     Report Count (1)
+    0x81, 0x06,                         //     Input (Data, Variable, Relative)
+    0xc0,                               //   END_COLLECTION
+    0xc0,                               // END_COLLECTION
+
+    //
     // Vendor defined control report starts here
     //
         0x06, 0x00, 0xff,                    // USAGE_PAGE (Vendor Defined Page 1)
--- FakerInput/fakerinputcommon.h
+++ FakerInput/fakerinputcommon.h
@@ -4,6 +4,7 @@
 #include <minwindef.h>
 
 #define REPORTID_KEYBOARD 0x02
+#define REPORTID_MOUSE          0x03
 #define REPORTID_RELATIVE_MOUSE 0x04
 #define REPORTID_ENHANCED_KEY   0x08
 #define REPORTID_CONTROL  0x40
