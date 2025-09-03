3b1235e7105d821ecf79cae2953afd916a4d0da8
Travis Nickles <ryochan7@protonmail.com>
2025-09-02 19:16:03 -0500

    Use a new device interface GUID

--- FakerInput/Public.h
+++ FakerInput/Public.h
@@ -22,9 +22,15 @@ Environment:
 // Define an Interface Guid so that apps can find the device and talk to it.
 //
 
-DEFINE_GUID (GUID_DEVINTERFACE_FakerInput,
-    0xab67b0fa,0xd0f5,0x4f60,0x81,0xf4,0x34,0x6e,0x18,0xfd,0x08,0x05);
-// {ab67b0fa-d0f5-4f60-81f4-346e18fd0805}
+// New GUID for FakerInput
+// {0E5AF65E-8134-425A-A5B7-8D97E60A3D60}
+DEFINE_GUID(GUID_DEVINTERFACE_FakerInput,
+    0xe5af65e, 0x8134, 0x425a, 0xa5, 0xb7, 0x8d, 0x97, 0xe6, 0xa, 0x3d, 0x60);
+
+// OLD GUID from vmulti
+//DEFINE_GUID (GUID_DEVINTERFACE_FakerInput,
+//    0xab67b0fa,0xd0f5,0x4f60,0x81,0xf4,0x34,0x6e,0x18,0xfd,0x08,0x05);
+//// {ab67b0fa-d0f5-4f60-81f4-346e18fd0805}
 
 #define MAXIMUM_STRING_LENGTH           (126 * sizeof(WCHAR))
 #define FAKERINPUT_MANUFACTURER_STRING    L"Ryochan7"  
